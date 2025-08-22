cc0be5763b464e13ab975fb46ea467b06de383c4
Rajib_Mahmud <rajib.ruet12@gmail.com>
2025-08-22 22:53:04 +0600

    Add files via upload
    assignment 1

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCsharp
+{
+    internal class Car
+    {
+        public string Brand;
+        public string Model;
+        public int Year;
+        public int Mileage;
+        public void Drive(int kilometers)
+        {
+            Mileage += kilometers;
+            Console.WriteLine("Car "+Brand+" "+Model+" has driven " + kilometers+" km.");
+            Console.WriteLine("Total mileage:"+Mileage+" km.");
+        }
+        public void CarInfo()
+        {
+            Console.WriteLine("Car Info:");
+            Console.WriteLine("Brand- " + Brand);
+            Console.WriteLine("Model- " + Model);
+            Console.WriteLine("Year- " + Year);
+            Console.WriteLine("Mileage- " + Mileage + " km");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LearningCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Toyota = new Car();
+            Car Tesla = new Car();
+
+            Toyota.Brand = "Toyota";
+            Toyota.Model = "Corolla";
+            Toyota.Year = 2020;
+            Toyota.Mileage = 15000;
+            Toyota.Drive(100);
+            Toyota.CarInfo();
+
+            Tesla.Brand = "Tesla";
+            Tesla.Model = "Model S";
+            Tesla.Year = 2021;
+            Tesla.Mileage = 5000;
+            Tesla.Drive(200);
+            Tesla.CarInfo();
+        }
+
+
+
+    }
+}
